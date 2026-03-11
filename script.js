90b90aa51b21c51b9b60d6f27d4fcdf9dc84a003
Hanabata <93629352+hanabata84@users.noreply.github.com>
2026-03-11 19:04:36 +0900

    test adding array library to DOM

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log(`hello there`);
+const bookLibrary = [
+    {
+        id: 1,
+        title: "The Hobbit",
+        author: "JRR Tolkien",
+        pages: 295,
+        read: true
+    }
+];
+
+function Book(id, title, author, pages, read) {
+    this.id = id,
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read
+}
+
+const table = document.querySelector('table')
+bookLibrary.forEach(book => {
+    const row = document.createElement('tr')
+
+    // title element
+    const titleCell = document.createElement('td')
+    titleCell.textContent = book.title
+    row.appendChild(titleCell)
+
+    // author element
+    const authorCell = document.createElement('td')
+    authorCell.textContent = book.author
+    row.appendChild(authorCell)
+
+    // pages element
+    const pagesCell = document.createElement('td')
+    pagesCell.textContent = book.pages
+    row.appendChild(pagesCell)
+
+    // read element
+    const readCell = document.createElement('td')
+    readCell.textContent = book.read
+    row.appendChild(readCell)
+
+    table.appendChild(row)
+});
